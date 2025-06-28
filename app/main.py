0ef3c58b6c6677c75277759057ee56fa660d95b4
Liliya Kyrylyshena <morsimmortalis05@gmail.com>
2025-06-28 13:31:39 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 class Animal:
-    health = 100
-    name = ""
-    hidden = False
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(
+            self, name: str, health: int = 100, hidden: bool = False
+    ) -> None:
         self.name = name
         self.health = health
-        self.alive.append(self)
+        self.hidden = hidden
+        Animal.alive.append(self)
 
-    def __repr__(self) -> None:
+    def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f"Health: {self.health}, "
                 f"Hidden: {self.hidden}}}")
@@ -23,7 +23,8 @@ class Herbivore(Animal):
 
 class Carnivore(Animal):
 
-    def bite(self, herbivore: Herbivore) -> None:
+    @staticmethod
+    def bite(herbivore: Herbivore) -> None:
 
         if not herbivore.hidden and isinstance(herbivore, Herbivore):
             herbivore.health -= 50
